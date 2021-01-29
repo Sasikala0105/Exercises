c23d1319599a653325086eb97017affb23c26ac7
USER <USER@LAPTOP-R3HUERQJ>
2021-01-29 23:52:24 +0800

    exercisesss

--- Exercise1/src/exam.java
+++
@@ -1,9 +0,0 @@
-
-public class exam {
-	char STIA113='A';
-
-	public void printExam() {
-		System.out.println("The result from method is " + STIA113); 
-		
-	}
-}
---
+++ exercise2/src/newMain.java
@@ -0,0 +1,29 @@
+
+
+public class newMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		personalinfo pi1 = new personalinfo();
+		 cgpa     obj1 = new cgpa();
+		 result r1 = new result(); 
+		 workExperience w2 = new workExperience();
+		 skills s = new skills();
+		
+			
+			
+			pi1.printPinfo();
+			System.out.println("\t");
+			obj1.printcgpa();
+			System.out.println("\t");
+			r1.printResult();
+			System.out.println("\t");
+			w2.printworkexperience();
+			System.out.println("\t");
+			s.printmyskills();
+			
+	 
+
+	}
+
+}
